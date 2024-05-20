64104f7a7b539b87e03556847fc4785ac0ebe65b
Song-dyong <brandy1930@nate.com>
2024-05-20 10:30:15 +0900

    init commit

---
+++ src/main/java/com/hh/backendmaterial/BackendMaterialApplication.java
@@ -0,0 +1,13 @@
+package com.hh.backendmaterial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMaterialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMaterialApplication.class, args);
+    }
+
+}
